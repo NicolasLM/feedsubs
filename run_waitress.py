4f8217f337ecf397f5e78eace5ff353295bfb307
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-03-03 12:49:41 +0100

    Remove run_waitress script
    There is now a Django command for that

--- run_waitress.py
+++
@@ -1,9 +0,0 @@
-import logging.config
-
-from django.conf import settings
-import waitress
-
-from feedsubs.wsgi import application as app
-
-logging.config.dictConfig(settings.LOGGING)
-waitress.serve(app=app, port=8000)
