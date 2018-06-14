0519fd7bb13dc2404fbaba3192d7b1f962e0856d
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-06-14 11:43:13 +0200

    Show session data in Admin

--- um/admin.py
+++ um/admin.py
@@ -1,4 +1,7 @@
+import pprint
+
 from django.contrib import admin
+from django.contrib.sessions.models import Session
 
 from . import models
 
@@ -6,3 +9,15 @@ from . import models
 @admin.register(models.UMProfile)
 class ProfileAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+
+    def _session_data(self, obj):
+        return pprint.pformat(obj.get_decoded())
+
+    _session_data.allow_tags = True
+    list_display = ['session_key', '_session_data', 'expire_date']
+    readonly_fields = ['session_key', '_session_data']
+    exclude = ['session_data']
