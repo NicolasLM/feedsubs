9a576eda9469a404386157b92b3e645a39ba3e8c
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-08-31 22:14:33 +0200

    Remove middlewares related to security
    In production everything these middlewares do is already taken care of
    by Caddy.

--- feedsubs/settings/base.py
+++ feedsubs/settings/base.py
@@ -52,13 +52,11 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'um.middleware.logout_deletion_pending'
 ]
 
