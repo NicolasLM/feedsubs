5ee1e49979c7514f98a03f4cc410f744b8234812
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-09-02 11:02:29 +0200

    Allow to close notifications

--- feedsubs/static/feedsubs/feedsubs.js
+++ feedsubs/static/feedsubs/feedsubs.js
@@ -125,3 +125,9 @@ $.ajaxSetup({
         }
     }
 });
+
+
+$(document).on('click', '.notification > button.delete', function() {
+    $(this).parent().addClass('is-hidden');
+    return false;
+});
