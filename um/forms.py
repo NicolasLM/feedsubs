fb0546c6b71727b4356918d50da4b715d06053bf
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-09-11 23:06:46 +0200

    Require users to agree to ToS when signing up

--- um/forms.py
+++ um/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
+
+from allauth.account.forms import SignupForm
 
 
 class DeleteUserForm(forms.Form):
@@ -14,3 +18,14 @@ class DeleteUserForm(forms.Form):
         cleaned_data = super().clean()
         if self._current_username != cleaned_data['username']:
             raise forms.ValidationError('Enter your current username')
+
+
+class SignupFormWithToS(SignupForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        tos_page = reverse_lazy('django.contrib.flatpages.views.flatpage',
+                                kwargs={'url': '/terms-of-service'})
+        label = mark_safe('I have read and I agree with the <a '
+                          'href="{}">Terms of Service</a>'.format(tos_page))
+        self.fields['tos'] = forms.BooleanField(required=True, label=label)
