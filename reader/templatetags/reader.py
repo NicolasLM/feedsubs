ae18dd284e38d48fd0d57f6fdc933b761d185bf5
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-09-14 12:01:25 +0200

    Rewrite relative links in feeds to absolute ones
    This fixes the display of images that use relative resources.

--- reader/templatetags/reader.py
+++ reader/templatetags/reader.py
@@ -1,5 +1,6 @@
 import hashlib
 from typing import Optional
+import urllib.parse
 
 import bleach
 import bs4
@@ -9,6 +10,10 @@ ALLOWED_TAGS = bleach.ALLOWED_TAGS + ['p', 'pre', 'img', 'br', 'h1', 'h2',
                                       'h3', 'h4', 'h5', 'h6']
 ALLOWED_ATTRIBUTES = {'img': ['src', 'title', 'alt']}
 ALLOWED_ATTRIBUTES.update(bleach.ALLOWED_ATTRIBUTES)
+URL_REWRITE_PAIRS = (
+    ('a', 'href'),
+    ('img', 'src')
+)
 register = template.Library()
 
 
@@ -33,8 +38,8 @@ def humanize_yearly_frequency(value: Optional[int]) -> str:
 
 
 @register.filter
-def clean_article(content: str) -> str:
-    """Clean and format an untrusted chunck of HTML.
+def clean_article(content: str, base_url: str=None) -> str:
+    """Clean and format an untrusted chunk of HTML.
 
     This filter cleans the HTML from dangerous tags and formats it so that
     it fits with the style of the surrounding document by shifting titles.
@@ -42,13 +47,32 @@ def clean_article(content: str) -> str:
     content = bleach.clean(
         content, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True
     )
-    return unify_style(content)
-
-
-def unify_style(content: str) -> str:
-    # The surrounding page uses h1 and h2, user content must start at h3
-    shift_by = 2
     soup = bs4.BeautifulSoup(content, 'html.parser')
+    unify_style(soup)
+    rewrite_relative_links(soup, base_url)
+    return soup.prettify()
+
+
+def rewrite_relative_links(soup: bs4.BeautifulSoup, base_url: str):
+    """Rewrite relative links in feeds to absolute ones.
+
+    Feeds should only contain absolute references to other resources. For feeds
+    using relative references, the best guess is to use the URL of the feed
+    as base resource.
+    """
+    for tag_name, attrib in URL_REWRITE_PAIRS:
+        for tag in soup.find_all(tag_name, attrs={attrib: True}):
+            tag[attrib] = urllib.parse.urljoin(base_url, tag[attrib])
+
+
+def unify_style(soup: bs4.BeautifulSoup):
+    """Unify hierarchy of titles in an HTML page.
+
+    Feeds often embed HTML using titles in an incorrect way, this function tries
+    to reorganize them so that the resulting page has a proper title hierarchy.
+    The surrounding page uses h1 and h2 so user content must start at h3.
+    """
+    shift_by = 2
 
     highest_title = 1
     for i in range(10, 0, -1):
@@ -61,8 +85,6 @@ def unify_style(content: str) -> str:
         title_tag_name = 'h{}'.format(i)
         for tag in soup.find_all(title_tag_name):
             tag.name = 'h{}'.format(i + shift_by)
-
-    return soup.prettify()
 
 
 @register.filter
