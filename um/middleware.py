c73f72bec0f879650a6d3728c3382297a73d8453
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-09-18 11:43:14 +0200

    Prevent non-staff users to see the admin interface

--- um/middleware.py
+++ um/middleware.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import logout
 from django.contrib import messages
+from django.http import Http404
+from django.urls import reverse
 
 from .background_messages import get_messages
 
@@ -12,9 +14,14 @@ def user_management_middleware(get_response):
     deletion, but the way to do that is not obvious.
     - Fetches Messages sent by background tasks and gives them to
     django.contrib.message framework.
+    - Prevent non-staff users to see the admin interface
     """
 
     def middleware(request):
+        if request.path.startswith(reverse('admin:index')):
+            if not request.user.is_staff:
+                raise Http404()
+
         if request.user.is_authenticated:
             if request.user.um_profile.deletion_pending:
                 messages.info(
@@ -24,6 +31,7 @@ def user_management_middleware(get_response):
                 )
                 logout(request)
             else:
+
                 for msg, level in get_messages(request.user):
                     messages.add_message(request, level, msg)
 
