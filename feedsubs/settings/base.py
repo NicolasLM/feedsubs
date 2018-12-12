d17dd82e706029fef2fd71cfaf3cf134ab9615ac
Nicolas Le Manchet <nicolas@lemanchet.fr>
2018-12-12 11:30:42 +0100

    Write logs to stdout instead of stderr

--- feedsubs/settings/base.py
+++ feedsubs/settings/base.py
@@ -186,6 +186,7 @@ LOGGING = {
         'console': {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',
             'formatter': 'default',
         },
     },
