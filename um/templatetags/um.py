889cfe877f0274a647f31faa15a740bc02bdc5c7
Nicolas Le Manchet <nicolas@lemanchet.fr>
2019-07-03 12:16:31 +0200

    Handle fetching background message when request is not available
    When a 500 is returned, the template does not have access to
    context['request']. In this case do not try to fetch background
    messages.

--- um/templatetags/um.py
+++ um/templatetags/um.py
@@ -8,5 +8,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def fetch_background_messages(context):
     """Template tag adding background messages to contrib message framework."""
-    add_background_messages_to_contrib_messages(context['request'])
+    try:
+        request = context['request']
+    except KeyError:
+        # It is possible in some cases that the request is not available
+        return ''
+
+    add_background_messages_to_contrib_messages(request)
     return ''
